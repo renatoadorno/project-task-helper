a867798ceefa8f1e9454c6f7de693c8497c516bc
renato <renatowillian.2000@gmail.com>
2022-07-05 12:17:52 -0300

    ✨ feat: add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { LogoHeader } from "./LogoHeader";
+
+export function Header() {
+  return (
+    <header className="w-full flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <LogoHeader />
+    </header>
+  )
+}
