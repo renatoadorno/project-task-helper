7ec2a96289cd7f7dad2725a42e876ab10c426370
renato <renatowillian.2000@gmail.com>
2022-07-05 12:15:59 -0300

    ✨ feat: add routes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Home } from "./pages/Home"
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/:route' element={<Login />} />
+      <Route path='/home/:status' element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
